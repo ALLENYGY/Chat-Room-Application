bf2a9cc360e163f79d536b33e633aceb336f2aa0
AllenYGY <allenygy@outlook.com>
2024-05-11 18:57:15 +0800

    Chat Room Project First Complete

---
+++ Chat/Client/Client.cpp
@@ -0,0 +1,310 @@
+﻿#include <winsock2.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <cstring>
+
+#pragma comment(lib, "ws2_32.lib")
+#define DEFAULT_PORT	5019
+
+using namespace std;
+
+string sendOperation();
+string renameOperation();
+string createGroupOperation(SOCKET sock);
+string addMemberOperation();
+string deleteMemberOperation();
+string addnewMemberOperation(string groupname);
+string deletenewMemberOperation(string groupname);
+string showRoomMember();
+string sendGroupOperation();
+void hintMessage();
+void sendData(SOCKET sock);
+void receiveData(SOCKET sock);
+void sendToServer(SOCKET sock, string sendBuff);
+bool checknumber(string num);
+string username;
+bool flag = true;
+
+int main(int argc, char** argv) {
+	WSADATA wsaData;
+	SOCKET connect_sock;
+	struct sockaddr_in server_addr;
+	char* server_name = "localhost";
+	unsigned short port = DEFAULT_PORT;
+
+	if (WSAStartup(0x202, &wsaData) == SOCKET_ERROR) {
+		fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Assume server is running on localhost
+
+	connect_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (connect_sock == INVALID_SOCKET) {
+		fprintf(stderr, "socket() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	if (connect(connect_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+		fprintf(stderr, "connect() failed with error %d\n", WSAGetLastError());
+		closesocket(connect_sock);
+		WSACleanup();
+		return -1;
+	}
+
+	printf("Connected to server: %s:%d successfully!\n", server_name, port);
+	printf("Type 'E' to close the connection\n");
+	string command=renameOperation();
+	sendToServer(connect_sock, command);
+	hintMessage();
+	// Start the threads for sending and receiving data
+	thread sendThread(sendData, connect_sock);
+	thread recvThread(receiveData, connect_sock);
+	// Wait for the send thread to finish before closing
+	sendThread.join();
+	recvThread.detach();  // Receive thread can be detached as it will exit on server close
+	// Cleanup
+	closesocket(connect_sock);
+	WSACleanup();
+	return 0;
+}
+
+
+void hintMessage() {
+	printf("------------------------------------------------------------------\n");
+	cout << "Hello," << username << "! Welcome to Chat Room!\n";
+	printf("------------------------------------------------------------------\n");
+	printf("Type 'R' to Rename yourself\n");						// Rename username
+	printf("Type 'S' to Send messages to a specific member\n");		// Send messages to a specific member
+	printf("Type 'E' to Exit\n");									// Quit the Chat Room
+	printf("Type 'L' to List all members in the chat room\n");		// List all members in the chat room
+	printf("Type 'C' to Create a new group\n");						// Create a new group
+	printf("Type 'A' to Add a member to the group\n");				// Add a member to the group
+	printf("Type 'D' to Delete a member from the group\n");			// Delete a member from the group
+	printf("Type 'G' to Send messages to the group\n");				// Send messages to the group
+	printf("------------------------------------------------------------------\n");
+}
+
+string sendOperation() {
+	string prefix = "S";
+	cout << "Enter the ID of the member you want to send a message to: \n";
+	string id; cin >> id;
+	if (!checknumber(id)) {
+		printf("Invalid ID.\n");
+		return "";
+	}
+	cout << "Enter the message you want to send:\n";
+	string msg; cin.ignore();
+	getline(cin,msg);
+	string command = prefix + "*" + id + "*" + msg + "*\0";
+	return command;
+}
+
+string sendGroupOperation() {
+	string prefix = "G";
+	cout << "Enter the Group Name: \n";
+	string gname; cin >> gname;
+	cout << "Enter the message you want to send:\n";
+	string msg; cin.ignore();
+	getline(cin, msg);
+	string command = prefix + "*" + gname + "*" + msg + "*\0";
+	return command;
+}
+
+string renameOperation() {
+	string prefix = "R";
+	printf("Enter your new name: ");
+	cin >> username;
+	string command = prefix + "*" + username + "*\0";
+	return command;
+}
+
+string createGroupOperation(SOCKET sock) {
+	string prefix = "C";
+	printf("Enter your Group name: ");
+	string groupname;
+	cin >> groupname;
+	string command = prefix + "*" + groupname+"*\0";
+	sendToServer(sock, command);
+	printf("Here is the member in the Chat room that You can invite!\n");
+	sendToServer(sock, showRoomMember());
+	sendToServer(sock, addnewMemberOperation(groupname));
+	printf("Group %s has been created.\n", groupname.c_str());
+	return command;
+}
+
+string addMemberOperation() {
+	printf("Enter the Group name:\n");
+	string groupname;
+	cin >> groupname;
+	string command= addnewMemberOperation(groupname);
+	return command;
+}
+
+string addnewMemberOperation(string groupname) {
+	string prefix = "A";
+	prefix += "*" + groupname + "*";
+	printf("Enter the UserID that you want to invite: (end with #)\n");
+	string id;
+	int count = 0;
+	string command = prefix + "*";
+	cin >> id;
+	if (!checknumber(id)) {
+		printf("Invalid ID.\n");
+		return "";
+	}
+	while (id != "#") {
+		command = command + id + "*";
+		count++;
+		cin >> id;
+		if (!checknumber(id) && id!="#") {
+			printf("Invalid ID.\n");
+			return "";
+		}
+	}
+	command += "*" + to_string(count) + "*\0";
+	return command;
+}
+
+string deleteMemberOperation() {
+	printf("Enter the Group name:\n");
+	string groupname;
+	cin >> groupname;
+	string command = deletenewMemberOperation(groupname);
+	return command;
+}
+
+
+string deletenewMemberOperation(string groupname) {
+	string prefix = "D";
+	prefix += "*"+ groupname + "*";
+	printf("Enter the UserID that you want to delete: (end with #)\n");
+	string id;
+	int count = 0;
+	string command = prefix + "*";
+	cin>> id;
+	if (!checknumber(id)) {
+		printf("Invalid ID.\n");
+		return "";
+	}
+	while (id != "#") {
+		command = command + id + "*";
+		count++;
+		cin >> id;
+		if (!checknumber(id) && id != "#") {
+			printf("Invalid ID.\n");
+			return "";
+		}
+	}	
+	command += "*" + to_string(count) + "*\0";
+	return command;
+}
+
+string showRoomMember() {
+	string prefix = "L";
+	string command = prefix + "\0";
+	return command;
+}
+
+void sendToServer(SOCKET sock,string sendBuff) {
+	int bytesSent;
+	bytesSent = send(sock, sendBuff.c_str(), sendBuff.length(), 0);
+	if (bytesSent == SOCKET_ERROR) {
+		fprintf(stderr, "send() failed with error %d\n", WSAGetLastError());
+		return;
+	}
+
+}
+
+void sendData(SOCKET sock) {
+	string sendBuff;
+	while (true) {
+		printf("Select your operation: Type 'H' to get help.\n");
+		char command;
+		cin >> command;
+		switch (command) {
+		case 'H':
+			hintMessage();
+			break;
+		case 'R':
+			sendBuff = renameOperation();
+			if(sendBuff!="")
+				sendToServer(sock, sendBuff);
+			break;
+		case 'S':
+			sendBuff = sendOperation();
+			if (sendBuff != "")
+				sendToServer(sock, sendBuff);
+			break;
+		case 'L':
+			sendBuff = "L\0";
+			sendToServer(sock, sendBuff);
+			break;
+		case 'E':
+			sendBuff = "E\0";
+			sendToServer(sock, sendBuff);
+			cout << "Bye!" << endl; flag = false;
+			return;
+		case 'C':
+			sendBuff = createGroupOperation(sock);
+			break;
+		case 'A':
+			if (sendBuff != "")
+				sendBuff = addMemberOperation();
+			sendToServer(sock, sendBuff);
+			break;
+		case 'D':
+			if (sendBuff != "")
+				sendBuff = deleteMemberOperation();
+			sendToServer(sock, sendBuff);
+			break;
+		case 'G':
+			sendBuff = sendGroupOperation();
+			sendToServer(sock, sendBuff);
+			break;
+		default:
+			printf("Invalid command.\n");
+			hintMessage();
+			break;
+		}
+	}
+}
+
+void receiveData(SOCKET sock) {
+	char recvBuff[100];
+	int bytesReceived;
+	while (true) {
+		memset(recvBuff, 0, sizeof(recvBuff));
+		bytesReceived = recv(sock, recvBuff, sizeof(recvBuff), 0);
+		if (bytesReceived > 0) {
+			printf("%s\n", recvBuff);
+		}
+		else if (bytesReceived == 0) {
+			printf("Server closed the connection\n");
+			break;
+		}
+		else {
+			if (flag) {
+				fprintf(stderr, "recv() failed with error %d\n", WSAGetLastError());
+				break;
+			}
+		}
+	}
+}
+
+bool checknumber(string num) {
+	for (int i = 0; i < num.length(); i++) {
+		if (num[i] < '0' || num[i] > '9') {
+			return false;
+		}
+	}
+	return true;
+}
---
+++ Chat/Server/Server.cpp
@@ -0,0 +1,379 @@
+﻿
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <process.h>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <algorithm>
+#include <winsock2.h>
+#include <iostream>
+
+#pragma comment(lib, "ws2_32.lib")
+#define DEFAULT_PORT	5019
+
+using namespace std;
+
+struct terminal {
+	int id;
+	bool isLogged;
+	string name;
+	SOCKET sock;
+	queue<string> messageQueue;  
+	mutex queueMutex;            // 互斥锁保护消息队列
+	condition_variable queueCond; // 条件变量用于通知消息可发送
+};
+
+struct group {
+	int id;
+	string name;
+	vector<int> members;
+};
+
+vector<terminal*> clients;  
+vector<group *> groups;
+mutex clientsMutex;
+
+void recv_message(terminal* client);
+void send_message(terminal* client);
+void handle_client(terminal* client);
+void create_group(terminal* sender, string groupName);
+void rename_client(terminal* sender, string newName);
+void add_member_to_group(terminal* sender, string groupId, int clientId);
+void remove_member_from_group(terminal* sender, string gname, int clientId);
+void disconnect_client(terminal* sender);
+void processMessage(terminal* sender, const char* buffer);
+
+int main(int argc, char** argv) {
+	struct sockaddr_in local;
+	SOCKET sock;
+	WSADATA wsaData;
+	if (WSAStartup(0x202, &wsaData) == SOCKET_ERROR)
+	{
+		// stderr: standard error are printed to the screen.
+		fprintf(stderr, "WSAStartup failed with error %d\n", WSAGetLastError());
+		//WSACleanup function terminates use of the Windows Sockets DLL. 
+		WSACleanup();
+		return -1;
+	}// end if
+
+	// Fill in the address structure
+	local.sin_family = AF_INET;
+	local.sin_addr.s_addr = INADDR_ANY;
+	local.sin_port = htons(DEFAULT_PORT);
+	sock = socket(AF_INET, SOCK_STREAM, 0);	//TCP socket
+
+
+	if (sock == INVALID_SOCKET)
+	{
+		fprintf(stderr, "socket() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	// bind the socket
+	if (bind(sock, (struct sockaddr*)&local, sizeof(local)) == SOCKET_ERROR)
+	{
+		fprintf(stderr, "bind() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+
+	// waiting for connections
+	if (listen(sock, 5) == SOCKET_ERROR)
+	{
+		fprintf(stderr, "listen() failed with error %d\n", WSAGetLastError());
+		WSACleanup();
+		return -1;
+	}
+	printf("Waiting for connections...\n");
+	int clientId = 1;
+	while (1) {
+		sockaddr_in clientAddr;
+		int clientSize = sizeof(clientAddr);
+		SOCKET client_socket = accept(sock, (struct sockaddr*)&clientAddr, &clientSize);
+		if (client_socket == INVALID_SOCKET) {
+			cerr << "Accept failed with error: " << WSAGetLastError() << endl;
+			continue;
+		}
+		// Create a name for the client append with clientId
+		string name= "Anonymous" + to_string(clientId);
+		terminal* newterminal = new terminal{ clientId++, true, name, client_socket };
+		// Ensure safe access to the global clients list
+		{
+			lock_guard<mutex> guard(clientsMutex);
+			clients.push_back(newterminal);
+		}
+		// Starting the client thread to handle incoming and outgoing messages
+		thread clientThread(handle_client, newterminal);
+		clientThread.detach();
+		cout << "Connected to client: " << inet_ntoa(clientAddr.sin_addr) << ":" << ntohs(clientAddr.sin_port) << endl;
+	}
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+}
+
+void handle_client(terminal* client) {
+	thread recvThread(recv_message, client);
+	thread sendThread(send_message, client);
+	recvThread.join();
+	sendThread.join(); 
+}
+
+void recv_message(terminal* client) {
+	char buffer[1024];
+	while (true) {
+		memset(buffer, 0, sizeof(buffer));
+		int bytesReceived = recv(client->sock, buffer, sizeof(buffer), 0);
+		//buffer[bytesReceived] = '\0';
+		if (bytesReceived == SOCKET_ERROR) {
+			cout << "Receive error for client " << client->name << ": " << WSAGetLastError() << endl;
+			break;
+		}
+		else if (bytesReceived == 0) {
+			cout << "Client " << client->name << " disconnected." << endl;
+			break;
+		}
+		else {
+			cout << "Receive From Client:" << client->name << ": " << buffer << endl;
+		}
+		processMessage(client, buffer);
+	}
+	client->isLogged = false;
+	closesocket(client->sock);
+}
+
+
+void send_message(terminal* client) {
+	unique_lock<mutex> lock(client->queueMutex);
+	while (client->isLogged || !client->messageQueue.empty()) {
+		client->queueCond.wait(lock, [&] { return !client->messageQueue.empty() || !client->isLogged; });
+		while (!client->messageQueue.empty()) {
+			string message = client->messageQueue.front();
+			client->messageQueue.pop();
+			send(client->sock, message.c_str(), message.length(), 0);
+		}
+	}
+}
+
+void sent_msg_to_specify_client(terminal* sender, int receiverID, const string& message) {
+	bool isFound = false;
+	terminal* targetClient = nullptr;
+	// Find Receiver Client
+	{
+		lock_guard<mutex> guard(clientsMutex);  // 使用 lock_guard 来保护 clients 列表
+		for (auto client : clients) {
+			if (client->id == receiverID) {
+				targetClient = client;
+				isFound = true;
+				break;
+			}
+		}
+	}
+	if (isFound && targetClient) {
+		unique_lock<mutex> lock(targetClient->queueMutex);
+		targetClient->messageQueue.push(message);
+		targetClient->queueCond.notify_one();
+	}
+	else {
+		unique_lock<mutex> lock(sender->queueMutex);
+		sender->messageQueue.push("Client Not Found!");
+		sender->queueCond.notify_one();
+	}
+}
+void sent_msg_to_specify_Group(terminal* sender,string groupname,string message) {
+	bool isFound = false;
+	{
+		lock_guard<mutex> guard(clientsMutex);  // 使用 lock_guard 保护对 groups 列表的访问
+		for (auto& g : groups) {
+			if (g->name == groupname) {
+				isFound = true;
+				for (auto& member : g->members) {
+					for (auto& client : clients) {
+						if (client->id == member) {
+							unique_lock<mutex> lock(client->queueMutex);
+							client->messageQueue.push(message);
+							client->queueCond.notify_one();
+						}
+					}
+				}	
+				break;
+			}
+		}
+	}
+	if (!isFound) {
+		unique_lock<mutex> lock(sender->queueMutex);
+		sender->messageQueue.push("Group Not Found!");
+		sender->queueCond.notify_one();
+	}
+}
+
+
+vector<string> split(const string& s, char delimiter) {
+	vector<string> tokens;
+	string token;
+	for (char ch : s) {
+		if (ch == delimiter) {
+			if (!token.empty()) {
+				tokens.push_back(token);
+				token.clear();
+			}
+		}
+		else {
+			token += ch;
+		}
+	}
+	if (!token.empty()) {
+		tokens.push_back(token);
+	}
+	return tokens;
+}
+
+
+void processMessage(terminal* sender, const char* buffer) {
+	vector<string> com = split(buffer, '*');
+	char op = com[0][0];
+	switch (op) {
+	case 'S': {
+		// Send message to a specific client
+		string msg= sender->name + ": " + com[2]+"\0";
+		sent_msg_to_specify_client(sender, stoi(com[1]), msg);
+		break;
+	}
+	case 'R':
+		// Change name
+		sender->name = com[1];
+		sent_msg_to_specify_client(sender, sender->id, "Change Your Name Successfully！");
+		break;
+	case 'L':{ 
+		// List all clients
+		lock_guard<mutex> guard(clientsMutex);
+		string clientList = "\nClient List:\n";
+		for (auto client : clients) {
+			clientList += to_string(client->id) + ": " + client->name + "\n";
+		}
+		unique_lock<mutex> lock(sender->queueMutex);
+		sender->messageQueue.push(clientList);
+		sender->queueCond.notify_one();
+		break;
+	}
+
+	case 'C':
+		create_group(sender, com[1]);
+		break;
+	case 'G': {
+		string msg = com[1] + ": " + sender->name + ": " + com[2] + "\0";
+		sent_msg_to_specify_Group(sender, com[1], msg);
+		break;
+	}
+	case 'A': {
+		// Add member to group
+		int size = com.size();
+		for(int i=2;i<size-1;i++)
+			add_member_to_group(sender, com[1], stoi(com[i]));
+		break;
+	}
+	case 'D': {
+		int size = com.size();
+		for (int i = 2; i < size - 1; i++)
+			remove_member_from_group(sender, com[1], stoi(com[i]));
+		break;
+	}
+	case 'E':
+		disconnect_client(sender);
+		break;
+	default:
+		unique_lock<mutex> lock(sender->queueMutex);
+		sender->messageQueue.push("Invalid operation. Please try again.");
+		sender->queueCond.notify_one();
+		break;
+	}
+}
+
+void create_group(terminal* sender, string groupName) {
+	int id = groups.size();
+	group* newgroup = new group{ id + 1, groupName, vector<int>{sender->id} };
+	groups.push_back(newgroup);
+}
+
+void add_member_to_group(terminal* sender, string groupname, int clientId) {
+	int size= groups.size();
+	for (int i = 0; i < size; i++) {
+		if (groups[i]->name == groupname){
+			if (sender->id != groups[i]->members[0]) {
+				string msg = "You are not the owner of the group (";
+				msg += groups[i]->name;
+				msg += ")\0";
+				sender->messageQueue.push(msg);
+				sender->queueCond.notify_one();
+				return;
+			}
+			if (find(groups[i]->members.begin(), groups[i]->members.end(), clientId) != groups[i]->members.end()) {
+				string msg = "The client is already in the group (";
+				msg += groups[i]->name;
+				msg += ")\0";
+				sender->messageQueue.push(msg);
+				sender->queueCond.notify_one();
+				return;
+			}
+			groups[i]->members.push_back(clientId);
+			string username;
+			for (auto client : clients) {
+				if (client->id == clientId) {
+					username = client->name;
+				}
+			}
+			string msg = "Add user " + username + " Successfully!\n\0";
+			sender->messageQueue.push(msg);
+			sender->queueCond.notify_one();
+			break;
+		}	
+	}
+}
+
+void remove_member_from_group(terminal* sender, string gname, int clientId) {
+	// Remove member from group
+	int size = groups.size();
+	for (int i = 0; i < size; i++) {
+		if (groups[i]->name == gname) {
+			if (sender->id != groups[i]->members[0]) {
+				string msg = "You are not the owner of the group (";
+				msg += groups[i]->name;
+				msg += ")\0";
+				sender->messageQueue.push(msg);
+				sender->queueCond.notify_one();
+				return;
+			}
+			// check if the clientID in the group
+			int groupsize = groups[i]->members.size();
+			bool isFounduser = false;
+			for (auto iter = groups[i]->members.begin(); iter != groups[i]->members.end(); iter++)
+			{
+				if (clientId == *iter)
+				{
+					// Remove the client from group
+					groups[i]->members.erase(iter);
+					isFounduser = true;
+					string msg = "Remove user Successfully!\0";
+					sender->messageQueue.push(msg);
+					sender->queueCond.notify_one();
+					break;
+				}
+			}
+			if (!isFounduser) {
+				string msg = "User Not Found!\0";
+				sender->messageQueue.push(msg);
+				sender->queueCond.notify_one();
+			}
+
+		}
+	}	
+}
+
+void disconnect_client(terminal* client) {
+	client->isLogged = false;
+	closesocket(client->sock);
+}
+
